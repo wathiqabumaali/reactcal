54d7bd7eb93508ff98aff37406c9cd81a7b4b4c9
wathiqabumaali <aeaabumaali@ucdavis.edu>
2019-08-13 15:20:19 -0700

    Initial commit

---
+++ app/assets/javascripts/components/appointments.jsx
@@ -0,0 +1,13 @@
+class Appointments extends React.Component{
+   constructor(props, context) {
+      super(props, context);
+    }
+   componentDidMount(){}
+   render() {
+      return (
+         <div>
+            <h1>CalReact</h1>
+         </div>
+      )
+   }
+}
